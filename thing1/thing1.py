8d00af307f2bb2a1a53ad8d4a0be748038ba441e
Motoko Wang <111317751+motokowang@users.noreply.github.com>
2022-10-11 10:37:59 -0700

    Initial

---
+++ thing1/thing1.py
@@ -0,0 +1,26 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from email.encoders import encode_7or8bit
+from flask import Flask, request, render_template
+from flask_cors import CORS, cross_origin
+
+# -----
+import pandas as pd
+import json
+import subprocess as sp
+
+from os import listdir, mkdir
+from os.path import isfile, join
+import os
+
+# Flask constructor takes the name of
+# current module (__name__) as argument.
+app = Flask(__name__)
+
+@app.route('/', methods=['POST', 'GET'])
+@cross_origin()
+def route_root(name=None):
+    return 'hello there from thing1'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=7776)
