0ab54911c41148fab490fc35f2b8cd0e3248988b
Motoko Wang <111317751+motokowang@users.noreply.github.com>
2022-10-11 23:32:58 -0700

    Basic DF functionality with MySQL DB

--- thing1/thing1.py
+++ thing1/thing1.py
@@ -6,6 +6,9 @@ from flask_cors import CORS, cross_origin
 # -----
 import pandas as pd
 import json
+#import mysql.connector as sql
+import sqlalchemy
+import pymysql
 
 # Flask constructor takes the name of
 # current module (__name__) as argument.
@@ -15,6 +18,13 @@ app = Flask(__name__)
 @cross_origin()
 def route_root(name=None):
     return 'hello there from thing1'
+
+@app.route('/student/<int:student_id>', methods=['GET'])
+@cross_origin()
+def route_student(student_id: int):
+    db_conn = sqlalchemy.create_engine('mysql+pymysql://root:yet_another_leaked_credential@bouncycastle:3306/playground')
+    df = pd.read_sql_query('select * from marks', db_conn)
+    return df[df.student_id == student_id].to_json()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=7776)
